7a9ef8b32ce0d46bec4c4d1dc66091a4f2d5f60b
Javier Sánchez Matías <javieriguv@hotmail.com>
2018-10-04 20:47:01 -0300

    Correccion de comentarios

--- OnTourSMV/Controller/PaginaPpal.aspx.cs
+++ OnTourSMV/Controller/PaginaPpal.aspx.cs
@@ -9,12 +9,13 @@ public partial class PaginaPpal : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        //Caso en que se esté intentando logearse sin usuarios(Forzadamente)
+        //Caso en que se ingresa mediante el login correctamente
+        
         if (Session["Usuario"] != null)
         {
             
         }
-        //Caso en que se ingresa mediante el login correctamente
+        //Caso en que se esté intentando logearse sin usuarios(Forzadamente)
         else
         {
             Response.Redirect("~/View/Login.aspx");

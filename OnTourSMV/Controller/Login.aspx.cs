1e05801392dbc7242bff235961da0796e934aa0e
Javier Sánchez Matías <javieriguv@hotmail.com>
2018-10-07 19:03:35 -0300

    Hasheo de claves en formato MD5 (Solo funcionarán los que se encuentren hasheados en la BD)

--- OnTourSMV/Controller/Login.aspx.cs
+++ OnTourSMV/Controller/Login.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Security.Cryptography;
 
 public partial class View_Login : System.Web.UI.Page
 {
@@ -11,7 +12,13 @@ public partial class View_Login : System.Web.UI.Page
     {
         //Se hace esto para que cuando se ingrese acá se borren las sesiones
         Session["Usuario"] = null;
-       
+
+        // Hasheo de claves para ponerlas en la BD
+        /*
+        string hashClave = Convert.ToBase64String(new System.Security.Cryptography.MD5CryptoServiceProvider().
+            ComputeHash(System.Text.Encoding.Default.GetBytes("luis")));
+        LabelAviso.Text += hashClave;
+        */
     }
 
     protected void btnLogin_Click(object sender, EventArgs e)
@@ -19,11 +26,14 @@ public partial class View_Login : System.Web.UI.Page
         //Los usuarios VIENEN creados desde cuando el cliente/empleado solicita/concreta el usuario/contrato
         try
         {
+
             EntitiesOnTour bd = new EntitiesOnTour();
             String usuario = txtUsuario.Text;
             String clave = txtClave.Text;
+            string hashClave = Convert.ToBase64String(new System.Security.Cryptography.MD5CryptoServiceProvider().
+            ComputeHash(System.Text.Encoding.Default.GetBytes(clave)));
             //Busca si el usuario existe en la base de datos (Hasheo no implementado aún)
-            if (bd.USUARIO.Any(it=> it.LOGIN_USR == usuario && it.PASS_USR == clave))
+            if (bd.USUARIO.Any(it=> it.LOGIN_USR == usuario && it.PASS_USR == hashClave))
             {
                 Session["Usuario"] = usuario;
                 Response.Redirect("~/View/PaginaPpal.aspx");

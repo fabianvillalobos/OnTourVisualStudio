593edf8b7340dcee062a99aa7a2485d30ae814d9
Fabian Villalobos <f_villalobos@live.cl>
2018-11-07 11:50:13 -0300

    Impresion a PDF en curso...

--- OnTourSMV/Controller/ListarContratos.aspx.cs
+++ OnTourSMV/Controller/ListarContratos.aspx.cs
@@ -1,9 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net;
+using System.Text;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using iTextSharp.text;
+using iTextSharp.text.html.simpleparser;
+using iTextSharp.text.pdf;
+using System.IO;
 
 public partial class ListarContratos : System.Web.UI.Page
 {
@@ -24,6 +31,28 @@ public partial class ListarContratos : System.Web.UI.Page
     {
         Response.Redirect("~/View/AgregarContratos.aspx");
     }
-  
 
+
+
+    /*
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        Response.Clear();
+        Response.ContentType = "application/pdf";
+        Response.Cache.SetCacheability(HttpCacheability.NoCache);
+
+        Document doc = new Document(PageSize.LETTER.Rotate(), 10, 10, 10, 10);
+        String path = this.Server.MapPath(".") + "Archivos\\MiArchivo.pdf";
+
+        FileStream file = new FileStream(path, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.ReadWrite);
+
+        PdfWriter.GetInstance(doc, file);
+        doc.Open();
+
+        doc.Add(new Paragraph(GridView1.ToString()));
+        doc.Close();
+
+        Process.Start(path);
+    }
+    */
 }

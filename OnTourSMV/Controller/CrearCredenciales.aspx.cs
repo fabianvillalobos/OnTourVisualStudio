30668bd8c9252e0bad9d40e64d92092eeac8f22f
Fabian Villalobos <f_villalobos@live.cl>
2018-11-18 11:08:17 -0300

    Creando Credenciales.

---
+++ OnTourSMV/Controller/CrearCredenciales.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class View_CrearCredenciales : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (Session["Usuario"] == null)
+        {
+            Response.Redirect("~/View/Login.aspx");
+        }
+        int perfilId = int.Parse(Session["PerfilID"].ToString());
+        if (perfilId != 2 || perfilId != 1) // Solo ejecutivos de ventas pueden agregar pasajeros (Clientes con cuentas asociarlos a contratos)
+        {
+            Response.Redirect("~/View/PaginaPpal.aspx");
+        }
+    }
+}

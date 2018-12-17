c0c7a8332197edd07cacac9ad1e1869d6a1cc6f2
l.ponces <l.ponces@alumnos.duoc.cl>
2018-12-17 05:17:01 +0100

    NuevaTransferencia - Ajustes y registro de Saldo en cuentas.

--- OnTourSMV/Controller/NuevaTransferencia.aspx.cs
+++ OnTourSMV/Controller/NuevaTransferencia.aspx.cs
@@ -32,7 +32,6 @@ public partial class NuevaTransferencia : System.Web.UI.Page
             EntitiesOnTour bd = new EntitiesOnTour();
             //DateTime fecha = new DateTime();
             int monto = int.Parse(txtMonto.Value);
-            System.Windows.Forms.MessageBox.Show(monto.ToString());
             pasoDos.Visible = true;
             txtMonto.Disabled = true;
             DropDownListTipo.Enabled = false;
@@ -46,12 +45,56 @@ public partial class NuevaTransferencia : System.Web.UI.Page
 
     protected void Notificar_Click(object sender, EventArgs e)
     {
+        
+        EntitiesOnTour bd = new EntitiesOnTour();
+        DateTime fecha = DateTime.Now;
+        int monto = int.Parse(txtMonto.Value);
+        int tipo = int.Parse(DropDownListTipo.SelectedValue);
+        string idContratoActual = Request.QueryString["id_contrato"];
+        decimal idContrato = int.Parse(idContratoActual);
+
+        decimal idusuario = decimal.Parse(Session["UsuarioID"].ToString());
+        CLIENTE cliente = bd.CLIENTE.FirstOrDefault(x => x.ID_USR == idusuario);
+        CUENTA cuentaEmisor = bd.CUENTA.FirstOrDefault(it => it.ID_CONTRATO == idContrato && it.NUMRUT_CLI == cliente.NUMRUT_CLI && it.ACTIVO.Equals("T"));
+
+        if (tipo == 1) 
+        {
+            cuentaEmisor.SALDO = cuentaEmisor.SALDO + monto;
+            bd.SaveChanges();
+        }
+        else 
+        {
+            //grupal
+            var v_cantidad = bd.CUENTA.Count(x => x.ID_CONTRATO == idContrato && x.ACTIVO.Equals("T"));
+            decimal saldo_prorrateado = monto / v_cantidad;
+            var cuentas = bd.CUENTA.Where(x => x.ID_CONTRATO == idContrato && x.ACTIVO.Equals("T")).ToList();
+            foreach (var cuenta in cuentas)
+            {
+                cuenta.SALDO = cuenta.SALDO + saldo_prorrateado;
+            }
+            bd.SaveChanges();
+        }
+
+        TRANSACCION transaccion = new TRANSACCION()
+        {
+            FECHA_TRANSACCION = fecha,
+            MONTO_TRANSACCION = monto,
+            ID_CUENTA = cuentaEmisor.ID_CUENTA,
+            ID_TIPO_TRANSACCION = tipo,
+            ACTIVO = "F"
+        };
+        bd.TRANSACCION.Add(transaccion);
+        bd.SaveChanges();
+
         System.Net.Mail.MailMessage mail = new System.Net.Mail.MailMessage();
         SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
         mail.From = new MailAddress("computin.ponce@gmail.com");
         mail.To.Add("l.ponces@alumnos.duoc.cl");
-        mail.Subject = "Transferencia en []";
-        mail.Body = "Se ha realizado una transferencia";
+        mail.Subject = "Transferencia en contrato: "+idContrato;
+        mail.Body = "Monto: "+monto+"\n"+
+            "Fecha: "+fecha+ "\n" +
+            "Rut de autor: "+cliente.NUMRUT_CLI+"-"+cliente.DRUT_CLI+ "\n" +
+            "Nombre: "+cliente.NOMBRE_CLIE+" "+cliente.APELLIDO_PAT_CLI;
 
         string strFileName = System.IO.Path.GetFileName(comprobante.PostedFile.FileName);
         Attachment attachFile = new Attachment(comprobante.PostedFile.InputStream, strFileName);
@@ -61,7 +104,8 @@ public partial class NuevaTransferencia : System.Web.UI.Page
         SmtpServer.Credentials = new System.Net.NetworkCredential("computin.ponce@gmail.com", "jeanette0513");
         SmtpServer.EnableSsl = true;
         SmtpServer.Send(mail);
-        System.Windows.Forms.MessageBox.Show("Enviado");
-        
+        System.Windows.Forms.MessageBox.Show("Confirmación enviada");
+
+        Page.Response.Redirect(Page.Request.Url.ToString(), true);
     }
 }
